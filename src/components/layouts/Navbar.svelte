9b781faea9b5f415bbe1fd5ad092be653e2578a2
Kadjo Sylvain <sylvainka12@gmail.com>
2020-05-02 17:58:47 +0000

    Add sidebar ans navbar components

---
+++ src/components/layouts/Navbar.svelte
@@ -0,0 +1,83 @@
+<style>
+  .nav {
+    grid-column: 2 / -1;
+    grid-row: 1 / 2;
+    background: #fff;
+    box-shadow: 0rem 0.5rem 0.8rem rgba(0, 0, 0, 0.05);
+  }
+
+  .nav__menu {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+  }
+
+  .nav__info {
+    display: flex;
+    align-items: center;
+  }
+
+  .nav__info-list {
+    display: flex;
+    align-items: center;
+  }
+
+  .nav__info-item.happy i {
+    color: #289f0b;
+  }
+
+  .nav__info-item {
+    font-size: 3rem;
+    padding: 0 1em;
+  }
+
+  .nav__info-item:not(:last-child) {
+    border-right: 2px solid #f4f4f4;
+  }
+
+  .nav__info-item i {
+    margin-right: 0.5rem;
+  }
+
+  .nav__info-item.cry i {
+    color: #b51818;
+  }
+
+  .nav__info-item.sad i {
+    color: #d3af36;
+  }
+
+  .nav__info-title {
+    margin-right: 0.5em;
+    font-size: 3rem;
+  }
+</style>
+
+<nav class="nav">
+  <div class="nav__menu container">
+    <div class="nav__info">
+      <h3 class="nav__info-title">
+        COTE D'IVOIRE
+        <span>:</span>
+      </h3>
+
+      <ul class="nav__info-list">
+        <li class="nav__info-item sad">
+          <i class="icofont-sad" />
+          <span class="nav__info-text">1333</span>
+        </li>
+        <li class="nav__info-item happy">
+          <i class="icofont-laughing" />
+          <span class="nav__info-text">527</span>
+        </li>
+
+        <li class="nav__info-item cry">
+          <i class="icofont-crying" />
+          <span class="nav__info-text">15</span>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ src/components/layouts/Sidebar.svelte
@@ -0,0 +1,95 @@
+<style>
+  .sidebar {
+    grid-column: 1 / 2;
+    grid-row: 1 / -1;
+    background: linear-gradient(
+      to bottom,
+      rgb(23, 75, 118),
+      rgba(45, 150, 236)
+    );
+
+    height: 100vh;
+  }
+
+  .sidebar__brand {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    font-size: 2rem;
+    padding: 1em 0;
+  }
+
+  .sidebar__menu {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+  }
+  .sidebar__item {
+    width: 100%;
+  }
+
+  .sidebar__link {
+    display: inline-block;
+    padding: 1em;
+    color: #fff;
+    text-transform: uppercase;
+    /* border: 1px solid; */
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    cursor: pointer;
+    transition: all 0.26s;
+  }
+
+  .sidebar__link:hover {
+    background: rgba(35, 59, 88, 0.2);
+  }
+
+  .sidebar__link.active {
+    background: #233b58;
+  }
+
+  .sidebar__link i {
+    font-size: 3.5rem;
+    margin-bottom: 0.2em;
+  }
+</style>
+
+<aside class="sidebar">
+  <div class="sidebar__brand">
+    <span>COVID19</span>
+  </div>
+
+  <ul class="sidebar__menu">
+
+    <li class="sidebar__item">
+      <span class="sidebar__link active">
+        <i class="icofont-ui-home" />
+        Dashborad
+      </span>
+    </li>
+    <li class="sidebar__item">
+      <span class="sidebar__link">
+        <i class="icofont-table" />
+        Resultats
+      </span>
+    </li>
+    <li class="sidebar__item">
+      <span class="sidebar__link">
+        <i class="icofont-map" />
+        Cartographie
+      </span>
+    </li>
+    <li class="sidebar__item">
+      <span class="sidebar__link">
+        <i class="icofont-chart-line" />
+        Graphique
+      </span>
+    </li>
+  </ul>
+</aside>
